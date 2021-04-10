69920ec44034cc89cae0dd3f5f255625e59f56c0
robinsonvs <robinsonvs@gmail.com>
2021-04-10 01:00:35 -0300

    created Service/Entity in CheckoutAPI

---
+++ checkout-api/src/main/java/com/severo/ecommerce/checkout/service/CheckoutService.java
@@ -0,0 +1,4 @@
+package com.severo.ecommerce.checkout.service;
+
+public interface CheckoutService {
+}
---
+++ checkout-api/src/main/java/com/severo/ecommerce/checkout/service/CheckoutServiceImpl.java
@@ -0,0 +1,7 @@
+package com.severo.ecommerce.checkout.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CheckoutServiceImpl implements CheckoutService{
+}
