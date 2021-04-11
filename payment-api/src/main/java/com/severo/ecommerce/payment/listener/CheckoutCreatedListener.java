27249afd0d892ef1c924895976afb34e2a492672
robinsonvs <robinsonvs@gmail.com>
2021-04-11 14:06:27 -0300

    implement processor and listener kafka p02

--- payment-api/src/main/java/com/severo/ecommerce/payment/listener/CheckoutCreatedListener.java
+++ payment-api/src/main/java/com/severo/ecommerce/payment/listener/CheckoutCreatedListener.java
@@ -1,10 +1,14 @@
 package com.severo.ecommerce.payment.listener;
 
 import com.severo.ecommerce.checkout.event.CheckoutCreatedEvent;
+import com.severo.ecommerce.payment.event.PaymentCreatedEvent;
 import com.severo.ecommerce.payment.streaming.CheckoutProcessor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
+
+import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
@@ -17,6 +21,12 @@ public class CheckoutCreatedListener {
         //payment process gateway
         //payment save database
         //send event to payment process
+        final PaymentCreatedEvent paymentCreatedEvent = PaymentCreatedEvent.newBuilder()
+                .setCheckoutCode(event.getCheckoutCode())
+                .setCheckoutStatus(event.getStatus())
+                .setPaymentCode(UUID.randomUUID().toString())
+                .build();
+        checkoutProcessor.output().send(MessageBuilder.withPayload(paymentCreatedEvent).build());
 
     }
 }
